740770d723e9ae92567b07af3066e67b98ea40b8
Elie <niyubwayoiraelie5777@gmail.com>
2024-11-21 13:03:28 +0200

    Eslint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-
 const Home = () => {
   return (
-    <h1 className="text-4xl text-center">Welcome to the world of Nextjs 15</h1>
+    <h1 className="text-4xl text-center">Welcome to the world Of Nextjs 15</h1>
   );
-}
+};
 
 export default Home;
