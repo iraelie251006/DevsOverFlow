096baa3b6fcde933baee27acbb1972550c3a4d7b
Elie <niyubwayoiraelie5777@gmail.com>
2024-11-28 17:02:36 +0200

    Implement Github SignIn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,23 @@
-const Home = () => {
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+
   return (
-    <h1 className="text-center text-4xl">Welcome to the world Of Nextjs 15</h1>
+    <>
+      <h1 className="text-center text-4xl">
+        Welcome to the world Of Nextjs 15
+      </h1>
+      <form className="flex h-screen items-center justify-center" action={async () => {
+        "use server";
+        await signOut({redirectTo: ROUTES.SIGN_IN})
+      }}>
+        <Button type="submit">Logout</Button>
+      </form>
+    </>
   );
 };
 
--- components/form/SocialAuth.tsx
+++ components/form/SocialAuth.tsx
@@ -1,37 +1,61 @@
-import Image from "next/image"
-import { Button } from "../ui/button"
+"use client";
+
+import Image from "next/image";
+import { signIn } from "next-auth/react";
+
+import ROUTES from "@/constants/routes";
+import { toast } from "@/hooks/use-toast";
+
+import { Button } from "../ui/button";
 
 const SocialAuth = () => {
-    const ButtonClass = "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5";
+  const ButtonClass =
+    "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5";
+
+  const handleSignIn = async (provider: "github" | "google") => {
+    try {
+      await signIn(provider, {
+        redirectTo: ROUTES.HOME,
+        redirect: false
+      });
+    } catch (error) {
+      console.log(error);
+
+      toast({
+        title: "Sign-in Failed",
+        description:
+          error instanceof Error
+            ? error.message
+            : "An Error Occured during Sign-in",
+        variant: "destructive",
+      });
+    }
+  };
 
   return (
     <div className="mt-10 flex flex-wrap gap-2.5">
-        <Button className={ButtonClass}>
-            <Image 
-             src="/icons/github.svg"
-             alt="Github Logo"
-             width={20}
-             height={20}
-             className="invert-colors mr-2.5 object-contain"
-            />
-            <span>
-                Login With Github
-            </span>
-        </Button>
-        <Button className={ButtonClass}>
-            <Image 
-             src="/icons/google.svg"
-             alt="Google Logo"
-             width={20}
-             height={20}
-             className="mr-2.5 object-contain"
-            />
-            <span>
-                Login With Google
-            </span>
-        </Button>
+      <Button className={ButtonClass} onClick={() => handleSignIn("github")}>
+        <Image
+          src="/icons/github.svg"
+          alt="Github Logo"
+          width={20}
+          height={20}
+          className="invert-colors mr-2.5 object-contain"
+        />
+        <span>Login With Github</span>
+      </Button>
+      <Button className={ButtonClass} onClick={() => handleSignIn("google")}>
+        <Image
+          src="/icons/google.svg"
+          alt="Google Logo"
+          width={20}
+          height={20}
+          className="mr-2.5 object-contain"
+        />
+        <span>Login With Google</span>
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default SocialAuth
+export default SocialAuth;
