fc6bb24d78f1bb44244e8c80a7734806ac29ca00
Elie <niyubwayoiraelie5777@gmail.com>
2024-11-30 20:32:41 +0200

    Create Mobile Navigation & Nav Links

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 
 import Theme from "./Theme";
+import MobileNavigation from "./MobileNavigation";
+import ROUTES from "@/constants/routes";
 
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
-      <Link href="/" className="flex items-center gap-1">
+      <Link href={ROUTES.HOME} className="flex items-center gap-1">
         <Image
           src="/images/site-logo.svg"
           width={23}
@@ -20,6 +22,7 @@ const Navbar = () => {
       <p>Global Search</p>
       <div className="flex-between gap-5">
         <Theme />
+        <MobileNavigation />
       </div>
     </nav>
   );
