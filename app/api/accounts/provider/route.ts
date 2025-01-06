2fed187e81ca6d8ec5841598e269b6f872aec430
Elie <niyubwayoiraelie5777@gmail.com>
2025-01-06 19:43:03 +0200

    Implement API Fetch Handler

--- app/api/accounts/provider/route.ts
+++ app/api/accounts/provider/route.ts
@@ -3,13 +3,17 @@ import { NextResponse } from "next/server";
 import Account from "@/database/account.model";
 import handleError from "@/lib/handlers/error";
 import { NotFoundError, ValidationError } from "@/lib/http-errors";
-import { AccountSchema} from "@/lib/validations";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validations";
 
 export const POST = async (request: Request) => {
   const { providerAccountId } = await request.json();
 
   try {
-    const validatedData = AccountSchema.partial().safeParse({ providerAccountId });
+    await dbConnect();
+    const validatedData = AccountSchema.partial().safeParse({
+      providerAccountId,
+    });
     if (!validatedData.success)
       throw new ValidationError(validatedData.error.flatten().fieldErrors);
 
---
+++ lib/handlers/fetch.ts
@@ -0,0 +1,47 @@
+import { RequestError } from "../http-errors";
+import logger from "../logger";
+import handleError from "./error";
+
+interface FetchOptions extends RequestInit {
+    timeout?: number
+}
+
+function isError(error: unknown): error is Error {
+    return error instanceof Error
+}
+export async function fetchHandler<T>(
+  url: string,
+  options: FetchOptions = {}
+): Promise<ActionResponse<T>> {
+    const { timeout = 5000, headers: customHeader = {}, ...restOptions } = options;
+
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), timeout);
+
+    const defaultHeaders: HeadersInit = {
+        "Content-Type": "application/json",
+        accept: "application/json",
+    }
+
+    const headers: HeadersInit = {...defaultHeaders, ...customHeader};
+    const config: RequestInit = {...restOptions, headers, signal: controller.signal};
+
+    try {
+        const response = await fetch(url, config);
+        clearTimeout(id)
+        if (!response.ok) {
+            throw new RequestError(response.status, `HTTP error: ${response.status}`)
+        }
+        return response.json();
+    } catch (err) {
+        const error = isError(err) ? err : new Error("Unknown Error")
+
+        if (error.name === "AbortError") {
+            logger.warn(`Request to ${url} was aborted due to timeout`)
+        } else {
+            logger.error(`Error fetching ${url}: ${error.message}`)
+        }
+
+        return handleError(error) as ActionResponse<T>
+    }
+}
