b44797a5c577ef61065ad9fa167e2fe34aec42a8
Elie <niyubwayoiraelie5777@gmail.com>
2025-03-10 01:20:14 +0200

    Create Tag Action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,8 +1,15 @@
+import { getTags } from "@/lib/actions/tag.action";
 
-const Tags = () => {
-  return (
-    <div>Tags</div>
-  )
-}
+const Tags = async () => {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 10,
+    query: "javascript",
+  });
 
-export default Tags
+  const { tags } = data || {};
+  console.log("Tags: ", JSON.stringify(tags, null, 2));
+  return <div>Tags</div>;
+};
+
+export default Tags;
