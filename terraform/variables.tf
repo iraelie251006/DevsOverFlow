88ccd145190f1dead10c863d9a6a7f8b6eafb5be
Irakoze Elie <niyubwayoiraelie5777@gmail.com>
2026-09-22 00:19:22 +0200

    added outputs and variables for EKS cluster configuration

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "cluster_name" {
+  default = "devflow-cluster"
+}
+
+variable "node_instance_type" {
+  default = "t3.medium"
+}
+
+variable "desired_nodes" {
+  default = 2
+}
