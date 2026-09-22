39c1a9903c446cd817067696a91c61eb73efd895
Irakoze Elie <niyubwayoiraelie5777@gmail.com>
2026-09-22 11:59:40 +0200

    updated Terraform provider versions and configurations for AWS, EKS, and variables

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -3,12 +3,17 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name = var.cluster_name
-  cluster_version = "1.29"
+  cluster_version = "1.32"
 
   vpc_id = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
+  create_kms_key            = false
+  cluster_encryption_config = {}
+
+  cluster_enabled_log_types = []
+  
   eks_managed_node_groups = {
     default = {
       instance_types = [var.node_instance_type]
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "cluster_name" {
 }
 
 variable "node_instance_type" {
-  default = "t3.medium"
+  default = "t3.small"
 }
 
 variable "desired_nodes" {
